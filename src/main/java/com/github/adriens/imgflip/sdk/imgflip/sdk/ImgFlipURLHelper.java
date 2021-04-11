30191faa26abca2914ac983a2b9ab0f596455b52
adriens <Adrien.Sales@gmail.com>
2021-04-11 11:59:26 +1100

    URL Helpers for top filters

--- src/main/java/com/github/adriens/imgflip/sdk/imgflip/sdk/ImgFlipURLHelper.java
+++ src/main/java/com/github/adriens/imgflip/sdk/imgflip/sdk/ImgFlipURLHelper.java
@@ -5,6 +5,14 @@
  */
 package com.github.adriens.imgflip.sdk.imgflip.sdk;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * A set of stativ helpers around Imgflip Urls
  * @author 3004SAL
@@ -29,12 +37,12 @@ public class ImgFlipURLHelper {
         return out;
     }
     public static String getPagePath(String inStream) {
-        return getPagePath(inStream, null);
+        return getPagePath(inStream, 1);
     }
     public static String getPagePath() {
         return getPagePath("fun");
     }
-    public static String getPagePath(String inStream, String sortOption) {
+    public static String getPagePath(String inStream, String sortOption, int page) {
         String out = "";
         if (inStream == null || inStream.isEmpty()) {
             out += "/m/" + HOTTEST_STREAMS.FUN.url();
@@ -46,6 +54,85 @@ public class ImgFlipURLHelper {
         } else {
             out += "?sort=" + sortOption;
         }
+        out += "&page=" + page;
         return out;
     }
+    
+    //////////////////////////////////////////////////////////////////////
+    // Top like queries
+    public static String getPathOfTop(String inStream, String sortOption, int page){
+        String out = IMGFLIP_ROOT_URL;
+        //ex: https://imgflip.com/m/fun?sort=top-1d
+        if (inStream == null || inStream.isEmpty()) {
+            out += "/m/" + HOTTEST_STREAMS.FUN.url();
+        } else {
+            out += "/m/" + inStream;
+        }
+        // now put the sort option
+        out += "?sort=" + sortOption;
+        out += "&page=" + page;
+        return out;
+    }
+     public static String getPathOfTop(String inStream, String sortOption){
+         return getPathOfTop(inStream, sortOption, 1);
+     }
+    public static String getPathOfTopOneDay(String stream){
+        return getPathOfTop(stream, "top-1d");
+    }
+    public static String getPathOfTopSevendays(String stream){
+        return getPathOfTop(stream, "top-7d");
+    }
+    public static String getPathOfTopThirtyDays(String stream){
+        return getPathOfTop(stream, "top-30d");
+    }
+    public static String getPathOfTopLastMonth(String stream, int page){
+        //top-2021-03
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, -1);
+        Date lDate = calendar.getTime();
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM");  
+        String sortOpt = "top-" + dateFormat.format(lDate);
+        return getPathOfTop(stream, sortOpt,page);
+    }
+    public static String getPathOfTopLastMonth(String stream){
+         return getPathOfTopLastMonth(stream, 1);
+    }
+    public static String getPathOfTopLastYear(String stream, int page){
+        //top-2021-03
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -1);
+        Date lDate = calendar.getTime();
+        
+        DateFormat dateFormat = new SimpleDateFormat("yyyy");  
+        String sortOpt = "top-" + dateFormat.format(lDate);
+        return getPathOfTop(stream, sortOpt, page);
+    }
+     public static String getPathOfTopLastYear(String stream){
+         return getPathOfTopLastYear(stream, 1);
+     }
+    public static String getPathOfTopOfMonthOfYear(String stream, int year, int month, int page){
+        String sortOpt = "top-" + year + "-" + StringUtils.leftPad(month+"", 2, "0");
+        return getPathOfTop(stream, sortOpt, page);
+    }
+    public static String getPathOfTopOfMonthOfYear(String stream, int year, int month){
+        return getPathOfTopOfMonthOfYear(stream, year, month, 1);
+    }
+    public static String getPathOfTopOfMonthOfYearWithPage(String stream, int year, int page){   
+        String sortOpt = "top-" + year;
+        return getPathOfTop(stream, sortOpt, page);
+    }
+    public static String getPathOfTopOfMonthOfYear(String stream, int year){
+        return getPathOfTopOfMonthOfYearWithPage(stream, year, 1);
+    }
+    
+    public static void main(String[] arg){
+        System.out.println("Path Fun for Last Month: <" + ImgFlipURLHelper.getPathOfTopLastMonth("fun")+ ">");
+        System.out.println("Path Fun for Last Year: <" + ImgFlipURLHelper.getPathOfTopLastYear("fun")+ ">");
+        System.out.println("Path Fun for Februray 2017: <" + ImgFlipURLHelper.getPathOfTopOfMonthOfYear("politics", 2017, 2) + ">");
+        System.exit(0);
+    }
+    
 }
